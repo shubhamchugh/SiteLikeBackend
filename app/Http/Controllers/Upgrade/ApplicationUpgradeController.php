b3b215d33cc25e0d18b990c1278374543a1d2ffe
Shubham chugh <chugh.shubham12@gmail.com>
2022-07-30 15:53:49 +0530

    missing namespace added

--- app/Http/Controllers/Upgrade/ApplicationUpgradeController.php
+++ app/Http/Controllers/Upgrade/ApplicationUpgradeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Upgrade;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Artisan;
 
 class ApplicationUpgradeController extends Controller
