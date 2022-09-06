bb19b474ecf3c69668458117067d408bcd4c4025
Shubham Chugh MacBook <chugh.shubham12@gmail.com>
2022-09-06 15:17:47 -0700

    dd removed

--- app/Http/Controllers/Scrape/WappalyzerScrapeController.php
+++ app/Http/Controllers/Scrape/WappalyzerScrapeController.php
@@ -31,9 +31,8 @@ class WappalyzerScrapeController extends Controller
 
         $primary_domain = Get_Domain::get_registrableDomain($domain->slug);
 
-        $wappalyzer = shell_exec("wappalyzer $primary_domain[httpUrl] 2>&1");
-       //$wappalyzer = shell_exec("pwd");
-        dd($wappalyzer);
+        $wappalyzer = shell_exec("wappalyzer $primary_domain[httpUrl]");
+      
         $wappalyzer = json_decode($wappalyzer, true);
 
         if (empty($wappalyzer['technologies']) && 'pending' !== $status) {
